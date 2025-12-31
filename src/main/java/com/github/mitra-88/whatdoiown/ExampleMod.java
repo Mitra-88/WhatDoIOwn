af591dd1279d1bee4350d937f17e36c583909eec
GitHub Action <action@github.com>
2025-12-31 21:34:41 +0000

    Initialize template repository proper
    Set package name to com.github.mitra-88.whatdoiown
    Set project name to WhatDoIOwn
    Set modid to whatdoiown

--- src/main/java/com/github/mitra-88/whatdoiown/ExampleMod.java
+++ src/main/java/com/github/mitra-88/whatdoiown/ExampleMod.java
@@ -1,11 +1,11 @@
-package com.example;
+package com.github.mitra-88.whatdoiown;
 
 import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.init.Blocks;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 
-@Mod(modid = "examplemod", useMetadata=true)
+@Mod(modid = "whatdoiown", useMetadata=true)
 public class ExampleMod {
     @Mod.EventHandler
     public void init(FMLInitializationEvent event) {
--- src/main/java/com/github/mitra-88/whatdoiown/init/AutoDiscoveryMixinPlugin.java
+++ src/main/java/com/github/mitra-88/whatdoiown/init/AutoDiscoveryMixinPlugin.java
@@ -1,4 +1,4 @@
-package com.example.init;
+package com.github.mitra-88.whatdoiown.init;
 
 import org.spongepowered.asm.lib.tree.ClassNode;
 import org.spongepowered.asm.mixin.extensibility.IMixinConfigPlugin;
