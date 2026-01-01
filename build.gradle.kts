9eb24384daf7fa0e27d81b914c73d31ffc1abba4
Mitra <120065660+Mitra-88@users.noreply.github.com>
2026-01-01 01:45:26 +0100

    revert back to gradle 8.8

--- build.gradle.kts
+++ build.gradle.kts
@@ -123,6 +123,9 @@ tasks.shadowJar {
             println("Copying dependencies into mod: ${it.files}")
         }
     }
+
+    // If you want to include other dependencies and shadow them, you can relocate them in here
+    fun relocate(name: String) = relocate(name, "$baseGroup.deps.$name")
 }
 
 tasks.assemble.get().dependsOn(tasks.remapJar)
